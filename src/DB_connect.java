bf0eb2ae83072868f5d0a6408237ede52eb49fbf
Richard Ringia <r.ringia99@gmail.com>
2021-01-18 19:34:25 +0100

    DB_connect

---
+++ src/DB_connect.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+class DB_connect {
+    private Connection connection;
+    public Statement stmt;
+    private ResultSet rs;
+
+    public DB_connect() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/unwdmi?useSSL=false", "root", "root");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void query(String query) {
+        try {
+            this.stmt = connection.createStatement();
+            System.out.println(query);
+            this.rs = this.stmt.executeQuery(query);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public PreparedStatement prepareQuery(String query) throws SQLException {
+        return connection.prepareStatement(query);
+    }
+
+    public ResultSet getResult() {
+        return this.rs;
+    }
+}
