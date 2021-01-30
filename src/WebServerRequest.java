92e0acc62eafb9b17fd9afa69d00e3c6465f02c4
robbinkok01@gmail.com <robbinkok01@gmail.com>
2021-01-30 11:57:48 +0100

    Changed humidity query to a select on a view

--- src/WebServerRequest.java
+++ src/WebServerRequest.java
@@ -13,7 +13,7 @@ public class WebServerRequest {
 
     private final String measurements = "SELECT * FROM `measurement`";
     private  final String windspeed = "SELECT DISTINCT(stations.name), AVG(measurement.wind_speed) FROM measurement JOIN stations ON stations.stn = measurement.station_stn WHERE stations.country LIKE '%CZECH REPUBLIC%' GROUP BY stations.name";
-    private final String hum = "SELECT DISTINCT(stations.stn), stations.name, measurement.temperature, measurement.dew_point, 100 - (5 * (measurement.temperature - measurement.dew_point)) AS Humidity FROM measurement JOIN stations ON stations.stn = measurement.station_stn WHERE measurement.dew_point != 0 AND measurement.dew_point < measurement.temperature ORDER BY Humidity DESC LIMIT 10";
+    private final String hum = "SELECT DISTINCT(name), country, stn, temperature, dew_point, 100 - (5 * (temperature - dew_point)) AS humidity FROM `europe` WHERE dew_point < temperature AND 'humidity' < 100  ORDER BY `humidity`  DESC LIMIT 10";
     private final String station_data = "SELECT DISTINCT stations.stn, stations.name, stations.longitude, stations.latitude, stations.elevation, measurement.station_stn, measurement.temperature, measurement.dew_point, measurement.wind_speed, measurement.wnddir FROM measurement JOIN stations ON stations.stn = measurement.station_stn WHERE stations.country=\"CZECH REPUBLIC\" OR stations.country=\"GERMANY\" OR stations.country=\"AUSTRIA\" OR stations.country=\"POLAND\" OR stations.country=\"SLOVAKIA\" GROUP BY stations.stn ";
     private final String stations = "SELECT * FROM `stations`";
 
@@ -76,3 +76,4 @@ public class WebServerRequest {
         return result;
     }
 }
+
